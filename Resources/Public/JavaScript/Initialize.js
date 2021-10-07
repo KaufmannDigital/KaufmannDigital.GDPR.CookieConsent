75f4a84f8c462878ca206304f20621a112dd346f
Niklas Droste <nd@kaufmann.digital>
2021-10-07 20:16:18 +0200

    Task: Change variable to documentNodeDisabled

--- Resources/Public/JavaScript/Initialize.js
+++ Resources/Public/JavaScript/Initialize.js
@@ -18,7 +18,7 @@ function loadCookiebannerHtml(openSettings = false) {
     xhr.send();
 }
 
-if (KD_GDPR_CC && KD_GDPR_CC.nodeTypeDisabled === false && document.cookie.indexOf(KD_GDPR_CC.cookieName) >= 0) {
+if (KD_GDPR_CC && KD_GDPR_CC.documentNodeDisabled === false && document.cookie.indexOf(KD_GDPR_CC.cookieName) >= 0) {
     /*Cookie set*/
     window.dataLayer = window.dataLayer || [];
     var cookieObject = JSON.parse(
@@ -55,7 +55,7 @@ if (KD_GDPR_CC && KD_GDPR_CC.nodeTypeDisabled === false && document.cookie.index
             consents: cookieObject.consents,
         },
     });
-} else if (KD_GDPR_CC.nodeTypeDisabled === false && document.getElementsByClassName('gdpr-cookieconsent-settings').length === 0 && window.neos === undefined) {
+} else if (KD_GDPR_CC.documentNodeDisabled === false && document.getElementsByClassName('gdpr-cookieconsent-settings').length === 0 && window.neos === undefined) {
     /*No Cookie set, not in backend & not on cookie page*/
     loadCookiebannerHtml();
 }
